b4ba6e81415ac9e800a4b7b6fcdf6026bf72b858
yashgupta228 <yashguptaa14@gmail.com>
2026-02-21 20:54:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,15 @@ st.title("KNN Weather Classification")
 X = np.array([[50, 70], [25, 80], [27, 60], [31, 65], [23, 85], [20, 75]])
 y= np.array([0, 1, 0, 0, 1, 1])
 label_map = {0: "Sunny", 1: "Rainy"}
-
 st.sidebar.header("Input Features")
 temp = st.sidebar.slider("Temperature", 10, 60, 26)
 hum = st.sidebar.slider("Humidity", 50, 95, 78)
-
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(X, y)
 new_data = np.array([[temp, hum]])
 prediction = knn.predict(new_data)[0]
 
 st.write(f"Predicted Weather: **{label_map[prediction]}**")
-
 fig, ax = plt.subplots()
 ax.scatter(X[y == 0, 0], X[y == 0, 1], color="orange", label="Sunny", s=100, edgecolor="k")
 ax.scatter(X[y == 1, 0], X[y == 1, 1], color="blue", label="Rainy", s=100, edgecolor="k")
